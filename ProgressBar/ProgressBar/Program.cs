0f5278c7d10c287210a92b35bb071deebd11aebc
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 18:45:22 +0200

    veikia

---
+++ ProgressBar/ProgressBar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ProgressBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+
+                Thread.Sleep(100);
+                System.Console.WriteLine("#");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pabaiga");
+            Console.ReadLine();
+
+        }
+    }
+}
