7746cfaa5e54dd4781e01d2f4a63cdd6c3a62638
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 19:29:09 +0200

    veikia

---
+++ savarankiskas2/savarankiskas2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius;
+            Console.WriteLine("Iveskite bet koki skaiciu:");
+            skaicius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Lyginiai skaiciai");
+            for (int i = 0; i < skaicius; i =+ 2)
+            {
+               
+                Console.WriteLine("{0}", i);
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
