567a64b22e80d8b94aa862c4a7d2f03b21e360b2
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 20:25:37 +0200

    savaites dienos

---
+++ Masyvai/Masyvai/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masyvai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] savaitesDienos =
+            {
+                "pirmadiens",
+                "antradienis",
+                "treciadienis",
+                "ketvirtadienis",
+                "penktadienis",
+                "sestadienis",
+                "sekmadienis"
+            };
+            //for (int i = 0; i < savaitesDienos.Length; i++)
+            //{
+            //    Console.WriteLine(savaitesDienos[i]);
+            //}
+            //Console.ReadLine();
+            Console.WriteLine("Pries:");
+            foreach (var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            savaitesDienos[2] = "TRECIADIENIS";
+            Console.WriteLine("");
+            Console.WriteLine("Po");
+            foreach (var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
