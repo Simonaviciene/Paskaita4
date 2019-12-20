f45f30e53210a7a188d5330738d24123faefa4dd
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 19:54:26 +0200

    veikia

---
+++ savarankiskas3/savarankiskas3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekstas;
+            
+            Console.WriteLine("Ivesti teksta:");
+
+            tekstas = Console.ReadLine();
+
+            for (int i = tekstas.Length - 1; i >= 0; i--)
+            {
+                char a = tekstas[i];
+                Console.Write("{0}", a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
