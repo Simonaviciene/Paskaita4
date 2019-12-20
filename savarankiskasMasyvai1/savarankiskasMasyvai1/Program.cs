f0a8413fe803d68aba4a1ce2739f29eeb61cc5b4
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 21:09:35 +0200

    mas

---
+++ savarankiskasMasyvai1/savarankiskasMasyvai1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskasMasyvai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciai = new int[5];
+
+            Console.WriteLine("ivesti 5 skaicius:");
+            
+
+            for (int i = 0; i <= skaiciai.Length - 1; i++)
+            {
+                int skaicius = Convert.ToInt32(Console.ReadLine());
+                skaiciai[i] = skaicius;
+               
+            }
+            for (int i = skaiciai.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine("{0}", skaiciai[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
