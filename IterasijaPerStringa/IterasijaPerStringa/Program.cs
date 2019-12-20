f41737896dbee28260ae795e6b6f4f4bfa18a284
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 18:37:58 +0200

    veikia

---
+++ IterasijaPerStringa/IterasijaPerStringa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterasijaPerStringa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "C#Kursas";
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                char current = vardas[i];
+                System.Console.WriteLine(current);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ iteracijuIndeksas/iteracijuIndeksas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iteracijuIndeksas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //iteracijos per simboliu diapazona.
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine(c);
+                
+            }
+           Console.ReadLine();
+        }
+    }
+}
