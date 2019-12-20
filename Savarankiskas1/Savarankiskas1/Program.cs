7fa487d0cd27a7e271770b5cdcfcaa189893ff26
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-20 19:12:55 +0200

    veikia

---
+++ Savarankiskas1/Savarankiskas1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius;
+            double skaiciuSuma = 0;
+            
+            Console.Write("Ivesti skaiciu: " );
+            skaicius = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 1; i <= skaicius; i++)
+            {
+                skaiciuSuma += i;
+            }
+            Console.WriteLine("skaiciu suma {0}", skaiciuSuma);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
