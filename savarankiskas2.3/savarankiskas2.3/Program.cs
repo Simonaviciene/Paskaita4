86a76bfe1cf114412f25ced395a5698d3259a218
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-29 22:59:51 +0200

    split append StringBuilder?

---
+++ savarankiskas2.3/savarankiskas2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sakinys = "Laimingu Naujuju Metu Visiems ";
+
+            string[] words = sakinys.Split(' '); //ka daro split?
+            StringBuilder s = new StringBuilder(); // StringBuilder?
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                s.Append(words[i]);
+                s.Append(" ");
+            }
+
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
