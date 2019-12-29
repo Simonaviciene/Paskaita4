bb11136dadfa630c3c2deef3b4d29b782fa788fc
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-29 21:36:44 +0200

    ("\n\n")?

---
+++ savarankiskas2.1/savarankiskas2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int sum;
+
+            sum = a + b;
+            Console.Write("\n\n");
+            Console.WriteLine("{0}", sum);
+            Console.Write("\n\n");
+
+            Console.ReadLine();
+
+        }
+    }
+}
