30f9b63b1e252026964481b195e6cb09104364de
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-01 22:59:44 +0200

    nebaigtas

---
+++ savarankiskasMasyvai2/savarankiskasMasyvai2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskasMasyvai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciai = new int[5];
+            int suma;
+                        
+            Console.WriteLine("Ivesti 5 skaicius:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                suma += skaiciai.Sum();
+                Console.WriteLine(suma);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
