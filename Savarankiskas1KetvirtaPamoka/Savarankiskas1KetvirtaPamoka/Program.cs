9f7951bae446c0dfc7b64a459c0c00ff185344bd
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-04 11:38:00 +0200

    veikia

--- Savarankiskas1KetvirtaPamoka/Savarankiskas1KetvirtaPamoka/Program.cs
+++ Savarankiskas1KetvirtaPamoka/Savarankiskas1KetvirtaPamoka/Program.cs
@@ -4,26 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Savarankiskas1
+namespace Savarankiskas1KetvirtaPamoka
 {
     class Program
     {
         static void Main(string[] args)
         {
             double skaicius;
-            double skaiciuSuma = 1;
-            
-            Console.Write("Ivesti skaiciu: " );
+            double skaiciuSuma = 0;
+
+            Console.Write("Ivesti skaiciu: ");
             skaicius = Convert.ToDouble(Console.ReadLine());
 
             for (int i = 1; i <= skaicius; i++)
             {
+                Console.Write("{0} ", i);
+                if (i < skaicius)
+                {
+                    Console.Write("+ ");
+
+                }
+                else
+                {
+                    Console.Write("= ");
+                }
+
                 skaiciuSuma += i;
             }
-            Console.WriteLine("skaiciu suma {0}", skaiciuSuma);
+            Console.WriteLine("{0}", skaiciuSuma);
             Console.ReadLine();
-            
-
         }
     }
 }
