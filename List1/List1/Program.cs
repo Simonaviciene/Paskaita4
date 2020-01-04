594611c45c3d3b2c038fbafbf0bb086e8e8ac341
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-04 12:38:57 +0200

    lyginiai skaiciai

---
+++ List1/List1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int ilgisSk = 10;
+
+            List<int> atsitiktiniaiSkaiciai = new List<int>();
+
+            Random atsitiktinis = new Random();
+
+            for (int i = 0; i < ilgisSk; i++)
+            {
+                atsitiktiniaiSkaiciai.Add(atsitiktinis.Next(1100));
+
+            }
+            for (int i = 0; i < ilgisSk; i++)
+            {
+                if (atsitiktiniaiSkaiciai[i] % 2 == 0)
+                {
+                    Console.WriteLine("{0}", atsitiktiniaiSkaiciai[i]);
+                }
+                
+            }
+
+            
+            Console.ReadLine();
+            
+            
+            
+
+
+
+           
+        }
+    }
+}
